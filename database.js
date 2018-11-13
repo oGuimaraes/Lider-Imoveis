80001f21a5e8653e2792c301e17c240c462029c3
oGuimaraes <otaviovgsr@gmail.com>
2018-11-13 00:06:26 -0200

    Estrutura básica criada

---
+++ database.js
@@ -0,0 +1,60 @@
+var imoveis =
+[
+    {
+        id: 01,
+        titulo: "Apartamento a venda no Serra",
+        tipo: "Apartamento",
+        categoria: "vender",
+        bairro: "Serra",
+        endereço: "Rua do Ouro, 682 - Belo Horizonte",
+        area: 70,
+        quartos: 2,
+        banheiro: 1,
+        vaga: 1,
+        valor: "215.00",
+        descricao: "Com armários nos quartos, com elevador. A aproximadamente 290 metros de O Boiadeiro Pizzaria."
+    },
+    {
+        id: 02,
+        titulo: "Apartamento à venda no Bandeirantes",
+        tipo: "Apartamento",
+        categoria: "vender",
+        bairro: "Bandeirantes",
+        endereço: "Rua Andrade, 572 - Belo Horizonte",
+        area: 62,
+        quartos: 2,
+        banheiro: 2,
+        vaga: 1,
+        valor: "580.00",
+        descricao: "Com elevador, com armários nos quartos, com varanda, apartamento com decoração. A aproximadamente 250 metros de Epa Supermercado."
+    },
+    {
+        id: 03,
+        titulo: "Apartamento para alugar no Liberdade",
+        tipo: "Apartamento",
+        categoria: "vender",
+        bairro: "Liberdade",
+        endereço: "Rua Orquida, 222 - Belo Horizonte",
+        area: 130,
+        quartos: 3,
+        banheiro: 4,
+        vaga: 3,
+        valor: "3.200,00",
+        descricao: "Que aceita permuta, com elevador, com varanda, com armários nos quartos. A aproximadamente 230 metros de Allia Gran Hotel."
+    },
+    {
+        id: 04,
+        titulo: "Apartamento para vender no Centro",
+        tipo: "Apartamento",
+        categoria: "vender",
+        bairro: "Centro",
+        endereço: "Rua São Paulo, 942 - Belo Horizonte",
+        area: 130,
+        quartos: 3,
+        banheiro: 4,
+        vaga: 3,
+        valor: "3.200,00",
+        descricao: "Que aceita permuta, com elevador, com varanda, com armários nos quartos. A aproximadamente 230 metros de Allia Gran Hotel."
+    },
+
+]
